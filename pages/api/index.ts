344892b8c0a05a1b97750c05a0f4b20b3700ea0d
adrianl01 <gustavo.adrian.leiva879@gmail.com>
2024-09-18 17:32:00 -0300

    third commit

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,9 +1,24 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { firestore } from "../../lib/firestore"
+import { User } from "../../lib/users";
+import { Auth } from "../../lib/auth";
+import { findOrCreateAuth } from "../../lib/controllers/auth";
+import { sendCode } from "../../lib/controllers/auth";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
     // const newUser = await firestore.collection("auth").add({
     //     email: "ga.l.1601@gmail.com"
     // })
-    res.send(process.env.FIREBASE_CONNECTION)
+
+    // const marce = new Auth("O74j21L7Yj6YpSqLcF9h")
+    // await marce.pull()
+    // marce.data.test = "updated data"
+    // await marce.push()
+    const auth = await sendCode(req.body.email)
+    // const newUser = await User.createNewUser({
+    //     email: "test+1"
+    // })
+    // newUser.data.test = "updated data"
+    // await newUser.push()
+    res.send(auth)
 }
